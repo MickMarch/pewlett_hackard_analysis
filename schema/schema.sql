1501f147c4a99e254ec37e08dde60599ee5f7f74
Mickadoodoo <michael.r.marchand@gmail.com>
2023-01-17 18:30:10 -0500

    Updated schema.sql

--- schema/schema.sql
+++ schema/schema.sql
@@ -41,13 +41,12 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL, 
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -55,3 +54,6 @@ CREATE TABLE dept_emp (
 	PRIMARY KEY (emp_no, dept_no)
 );
 
+-- DROP TABLE;
+
+SELECT * FROM employees;
